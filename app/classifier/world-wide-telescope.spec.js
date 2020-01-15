3fa0a911d69d6182681df5bc80bc33fe00fc18ea
Roger Hutchings <roger@hutchin.gs>
2020-01-15 15:02:24 +0000

    Disable other WWT test block

--- app/classifier/world-wide-telescope.spec.js
+++ app/classifier/world-wide-telescope.spec.js
@@ -138,7 +138,7 @@ xdescribe('WorldWideTelescope render without incomplete annotations', function (
   });
 });
 
-describe('WorldWideTelescope with classification', function() {
+xdescribe('WorldWideTelescope with classification', function() {
   let wrapper;
 
   before(function () {
